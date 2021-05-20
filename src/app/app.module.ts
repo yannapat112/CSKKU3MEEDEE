085257f8c1dabd827e2961ae5b3781eab490b9a1
poowadolracbanjoeb <poowadol@kkumail.com>
2021-05-20 10:35:31 +0700

    xyz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NtComponent } from './nt/nt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NtComponent
   ],
   imports: [
     BrowserModule,
