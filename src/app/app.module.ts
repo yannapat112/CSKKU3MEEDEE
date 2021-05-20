b79639146dd0b92ab08634c8679e6e044e440f85
yannapat112 <yannapat@kkumail.com>
2021-05-20 10:30:17 +0700

    i o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Project1Component } from './project1/project1.component';
+import { Project2Component } from './project2/project2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Project1Component
+    Project1Component,
+    Project2Component
   ],
   imports: [
     BrowserModule,
