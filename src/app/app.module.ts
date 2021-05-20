072fa249e0315896e4f29c0db195ea396c4e01ea
chatchai12 <chatchaiCH@kkumail.com>
2021-05-20 11:14:14 +0700

    testnaja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Project1Component } from './project1/project1.component';
-<<<<<<< HEAD
 import { WorkComponent } from './components/work/work.component';
-=======
 import { ChatchaicomponentComponent } from './chatchaicomponent/chatchaicomponent.component';
->>>>>>> fileswork
 
 @NgModule({
   declarations: [
     AppComponent,
     Project1Component,
-<<<<<<< HEAD
-    WorkComponent
-=======
+    WorkComponent,
     ChatchaicomponentComponent
->>>>>>> fileswork
   ],
   imports: [
     BrowserModule,
