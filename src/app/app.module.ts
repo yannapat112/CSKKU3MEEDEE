b0e1daa4b6ef043eed3f1b8122420faaf2b48d7a
chatchai12 <chatchaiCH@kkumail.com>
2021-05-20 10:23:39 +0700

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Project1Component } from './project1/project1.component';
+import { ChatchaicomponentComponent } from './chatchaicomponent/chatchaicomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Project1Component
+    Project1Component,
+    ChatchaicomponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chatchaicomponent/chatchaicomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatchaicomponent',
+  templateUrl: './chatchaicomponent.component.html',
+  styleUrls: ['./chatchaicomponent.component.css']
+})
+export class ChatchaicomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
